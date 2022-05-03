da1b2422c880cf12455a745a96a47d72a00cdaec
KamauDev-maker <oscarnjenga@gmail.com>
2022-05-03 10:28:50 +0300

    making the Api call

--- app/views.py
+++ app/views.py
@@ -1,13 +1,16 @@
 from flask import render_template
 from app import app
+from .request import get_sources
 
 @app.route('/')
 def index():
     """
     view root page function that returns the index page and its data
     """
+    sources = get_sources()
+    breaking_news = get_breaking_news()
     title = 'The E-news'
-    return render_template('index.html',title = title)
+    return render_template('index.html',title = title,sources=sources,breaking_news=breaking_news)
 
 @app.route('/articles/<articles_id>')
 def  articles(articles_id):

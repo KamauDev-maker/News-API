12d1437e06f17dd46fa9e6a770070fbee9a02625
KamauDev-maker <oscarnjenga@gmail.com>
2022-05-03 10:10:15 +0300

    creating the first APIcall

--- app/request.py
+++ app/request.py
@@ -1,3 +1,23 @@
 from app import app
+import urllib.request,json
+from .models import Sources
 
 api_key = app.config['NEWS_API_KEY']
+base_url = app.config["NEWS_API_BASE_URL"]
+
+def get_sources():
+    '''
+    function that gets the json response to our url request
+    '''
+    get_sources_url = base_url.format(api_key)
+    with urllib.request.urlopen(get_sources_url) as url:
+        get_sources_data = url.read()
+        get_sources_response = json.loads(get_sources_data)
+        
+        sources_results = None
+        
+        if get_sources_response['sources']:
+            sources_results_list = get_sources_response['sources']
+            sources_results = process_sources(sources_results_list)
+    return sources_results        
+            

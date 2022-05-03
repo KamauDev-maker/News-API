e94502d28e41525810f184961ab3c8c3dbad2db6
KamauDev-maker <oscarnjenga@gmail.com>
2022-05-03 12:59:58 +0300

    rewriting the new application structure

--- app/config.py
+++ app/config.py
@@ -1,8 +1,13 @@
+import os
+
 class Config:
     """
     General configuration parent class
     """
     NEWS_API_BASE_URL = 'https://newsapi.org/v2/top-headlines/sources?{}apiKey={}'
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY') 
+
 
 class ProdConfig (Config):
     """
@@ -22,3 +27,8 @@ class DevConfig(Config):
     """
     
     DEBUG = True
+    
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,8 @@
-from flask import render_template
-from app import app
-from .request import get_sources
+from flask import render_template,request,redirect,url_for
+from . import main
+from ..request import get_sources,get_top_articles,get_keyword,get_breaking_news
 
-@app.route('/')
+@main.route('/')
 def index():
     """
     view root page function that returns the index page and its data
@@ -10,12 +10,40 @@ def index():
     sources = get_sources()
     breaking_news = get_breaking_news()
     title = 'The E-news'
-    return render_template('index.html',title = title,sources=sources,breaking_news=breaking_news)
+    get_keyword = request.args.get('keyword_query')
+    
+    if get_keyword:
+        return redirect(url_for('main.keyword',keyword_name=get_keyword))
+    else:
+        return render_template('index.html',title = title,sources=sources,breaking_news=breaking_news)
 
-@app.route('/articles/<articles_id>')
-def  articles(articles_id):
+
+@main.route('/source/<sourcesId>')
+def  articles(sourcesId):
     """
     view function that displays details of the articles from a particular source
     """
-    return render_template('articles.html',id = articles_id)
+    articles = get_top_articles(sourcesId)
+    title = f"{sourcesId}"
+    header = sourcesId.upper()
+    get_keyword =request.args.get('keyword_query')
+    
+    if get_keyword:
+        return redirect(url_for('main.keyword', keyword_name=get_keyword))
+    else:     
+        return render_template('articles.html',title = title,header=header,articles=articles)
+    
+@main.route('/search/<keyword_name>')
+def keyword(keyword_name):
+    '''
+    View function to display the search results
+    '''
+    keyword_name_list = keyword_name.split(" ")
+    keyword_name_format = "+".join(keyword_name_list)
+    searched_keyword = get_keyword(keyword_name_format)
+    title = f'{keyword_name.title()}'
+    display_keyword = keyword_name.upper()
+
+    return render_template('search.html', keyword = searched_keyword, title = title, display_keyword=display_keyword )
+    
     
